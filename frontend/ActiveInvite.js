2b8237551f5d3996a7d10d140e0d1dadf9a4066a
Emma Jia <emmajia@unc.edu>
2023-10-28 15:41:57 -0400

    list invites

---
+++ frontend/ActiveInvite.js
@@ -0,0 +1,10 @@
+const ActiveInvite = ({ navigation }) => {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Active Invitations</Text>
+  
+      </View>
+    );
+  };
+  
+  export default ActiveInvite;
--- frontend/HomePage.js
+++ frontend/HomePage.js
@@ -2,10 +2,58 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 
+dummy = [
+  {
+  username: 'kc',
+  phone: '123'
+  },
+  {
+    username: 'hi',
+    phone: '123'
+    },
+  {
+    username: 'hello',
+    phone: '123'
+    },
+]
+
+const handleYesButton = () => {
+  alert('Yes pressed')
+};
+
+const handleNoButton = () => {
+  alert('No pressed')
+};
+
+const handleStopButton = () => {
+  alert('Stop pressed')
+};
+
+
+const invites = dummy.map((user, i) => (
+  <View>
+    <Text>{user.username}</Text>
+    <Text>{user.phone}</Text>
+    <Button 
+      title="Yes"
+      onPress={handleYesButton}
+      />
+    <Button 
+      title="No"
+      onPress={handleNoButton}
+      />
+    <Button 
+      title="STOP"
+      onPress={handleStopButton}
+      />
+  </View>
+));
+
 const HomePage = ({ navigation }) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to the Home Page!</Text>
+      <Text>Active Invitations</Text>
+      {invites}
     </View>
   );
 };
